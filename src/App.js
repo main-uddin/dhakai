c9b068b4a343667cfcd386321e22e6f30487d2fe
main-uddin <mdmain74@gmail.com>
2022-02-15 16:31:47 +0600

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
 import Home from "./components/home/Home";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
-import { useState } from "react/cjs/react.development";
 
 function App() {
-  const [isAuth, setAuth] = useState(true);
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={isAuth ? <Home /> : <Navigate to="/" />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
