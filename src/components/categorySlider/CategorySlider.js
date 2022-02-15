ad62abe17a221d91938d317562263ca34c8c54ba
main-uddin <mdmain74@gmail.com>
2022-02-15 15:28:03 +0600

    remove unused code

--- src/components/categorySlider/CategorySlider.js
+++ src/components/categorySlider/CategorySlider.js
@@ -1,16 +1,5 @@
-// import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
-
 import "./CategorySlider.css";
 const CategorySlider = () => {
-  // const settings = {
-  //   dots: false,
-  //   infinite: true,
-  //   speed: 500,
-  //   slidesToShow: 5,
-  //   slidesToScroll: 1,
-  // };
   return (
     <div className="slider-wrapper">
       <div className="rectangle-bg">All for you</div>
